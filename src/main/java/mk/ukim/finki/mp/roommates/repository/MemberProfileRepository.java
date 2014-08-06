90e60e92ab6aa5317cb45959b27445989e56bd6f
RosanaAlcheva <alcheva_rosana@yahoo.com>
2014-08-06 19:54:52 +0200

    Added repositories

--- src/main/java/mk/ukim/finki/mp/roommates/repository/MemberProfileRepository.java
+++ src/main/java/mk/ukim/finki/mp/roommates/repository/MemberProfileRepository.java
@@ -1,9 +1,9 @@
 package mk.ukim.finki.mp.roommates.repository;
 
-import mk.ukim.finki.mp.roommates.model.Member;
+import mk.ukim.finki.mp.roommates.model.MemberProfile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberProfileRepository extends JpaRepository<Member, Long> {
+public interface MemberProfileRepository extends JpaRepository<MemberProfile, Long> {
 	
 }
