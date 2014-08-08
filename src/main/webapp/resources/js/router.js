1bf7c89c3a97256d55444f26980d2cc277153d37
NCuculova <nadica_cuculova@yahoo.com>
2014-08-08 20:42:19 +0200

    bla bla bu

---
+++ src/main/webapp/resources/js/router.js
@@ -0,0 +1,11 @@
+'use strict';
+
+RM.config([ '$routeProvider', '$locationProvider',
+		function($routeProvider, $locationProvider) {
+			$routeProvider.when('/members', {
+				templateUrl : 'views/member.html',
+				controller: 'MembersController'
+			}).when('/memberProfile',{
+				templateUrl : 'views/memberProfile.html'
+			});
+		} ]);
---
+++ src/main/webapp/resources/js/services.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Services */
+
+angular.module('RM.services', [ 'chieffancypants.loadingBar' ]).value(
+		'version', '0.1');
+
+RM.factory('Member', [ '$resource', function($resource) {
+	return $resource(WPUtil.ctx('/data/rest/members/:id'), {});
+} ]);
+
+RM.factory('MemberProfile', [ '$resource', function($resource) {
+	return $resource(WPUtil.ctx('/data/rest/memberProfile/:id'), {});
+} ]);
